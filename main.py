d45e4edfdf767656e9c73ac0aa97403d30a8cf15
b <leshasadretdinov@gmail.com>
2026-01-18 21:01:29 +0300

    raarar

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print('Привет')   # просто  print()
-print()
+import hashlib
+
+password = "pas123"
+hashed_password = hashlib.sha256(password.encode()).hexdigest()
+if 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f' == hashed_password:
+    print('yes')
+else:
+    print(hashed_password)
+    print('no')
