fffa1f9a208886bb1dc7c73ce9d706f359c4ead3
b <leshasadretdinov@gmail.com>
2026-01-18 21:04:43 +0300

    3

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ class mainWindow(QMainWindow):
         self.hide()
         self.next = Memo()
         self.next.show()
-
+print()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
